b9eab8903a9b6a819a6d04b61c634a123021b9a6
jguo <jg4006@columbia.edu>
2025-04-20 19:13:52 -0400

    initial commit of shallow

---
+++ vxl_importer.py
@@ -0,0 +1,40 @@
+# goxel https://github.com/guillaumechereau/goxel/releases/tag/v0.15.1
+
+import numpy as np
+
+def import_vxl(fpath):
+    xmin = ymin = zmin = 9999
+    xmax = ymax = zmax = -9999
+    lines = []
+    with open(fpath, 'r') as f:
+        for line in f.readlines():
+            if line.startswith('#'):
+                continue
+            x, y, z, color = line.split(' ')
+            x = int(x)
+            y = int(y)
+            z = int(z)
+            color = color.strip('\n')
+            lines.append((x, y, z, color))
+
+    # 000000 determines base size
+    for x, y, z, color in lines:
+        if color == '000000':
+            xmin = min(xmin, x)
+            ymin = min(ymin, y)
+            zmin = min(zmin, z)
+            xmax = max(xmax, x)
+            ymax = max(ymax, y)
+            zmax = max(zmax, z)
+
+    terrain = np.zeros([xmax - xmin + 1, ymax - ymin + 1])
+    # 8f563b is terrain
+    for x, y, z, color in lines:
+        if color == '8f563b':
+            terrain[x - xmin, y - ymin] = max(terrain[x - xmin, y - ymin], z - zmax)
+
+    water = np.zeros([xmax - xmin + 1, ymax - ymin + 1])
+    for x, y, z, color in lines:
+        if color == '639bff':
+            water[x - xmin, y-ymin] = max(water[x - xmin, y-ymin], z - terrain[x - xmin, y - ymin])
+    return terrain, water
