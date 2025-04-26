eff94304173e06ba7b7b651a40716725ccb9bd15
jguo <jg4006@columbia.edu>
2025-04-26 12:58:25 -0400

    single step & pause

--- main.py
+++ main.py
@@ -1,23 +1,16 @@
 import matplotlib.pyplot as plt
-import numpy as np
-import tqdm
+from water import Simulator
 
-from vxl_importer import import_vxl
+simulation = Simulator(r'data\lake_fill.txt')
 
-terrain, water = import_vxl(r'data\lake_fill.txt')
-N = terrain.shape[0]
-flowx = np.zeros([N + 1, N])
-flowy = np.zeros([N, N + 1])
-frictionFactor = 0.5
-g = 10
-dx = dy = 10
-dt = 1
-
-paused = False
+paused = True
+trigger = False  # single-step
 def on_press(event):
-    global paused
+    global paused, trigger
     if event.key == 'p':
         paused = not paused
+    elif event.key == 'a':
+        trigger = True
 
 def on_close(event):
     exit(0)
@@ -25,53 +18,19 @@ def on_close(event):
 fig, ax = plt.subplots()
 fig.canvas.mpl_connect('key_press_event', on_press)
 fig.canvas.mpl_connect('close_event', on_close)
+step = 0
 
-for step in tqdm.tqdm(range(100000)):
-    if paused:
+while True:
+    ax.imshow(simulation.water, cmap='hot', interpolation='nearest')
+    ax.set_title(str(step))
+    #plt.show()
+    plt.pause(0.01)
+
+    if not trigger and paused:
         plt.pause(1)
         continue
 
-    flowx[0, :] = 0
-    flowx[N, :] = 0
-    flowy[:, 0] = 0
-    flowy[:, N] = 0
-
-    for y in range(N):
-        for x in range(1, N):
-            flowx[x, y] = flowx[x, y] * frictionFactor + \
-                          ((water[x - 1, y] + terrain[x - 1, y]) - (water[x, y] + terrain[x, y])) * g * dt / dx
-
-    for y in range(1, N):
-        for x in range(N):
-            flowy[x, y] = flowy[x, y] * frictionFactor + \
-                          ((water[x, y - 1] + terrain[x, y - 1]) - (water[x, y] + terrain[x, y])) * g * dt / dx
-
-    for y in range(N):
-        for x in range(N):
-            total_outflow = 0
-            total_outflow += max(0, -flowx[x,y])
-            total_outflow += max(0, -flowy[x,y])
-            total_outflow += max(0, flowx[x+1,y])
-            total_outflow += max(0, flowy[x,y+1])
-
-            if total_outflow > 0:
-                max_outflow = water[x, y] * dx * dy / dt
-                scale = min(1, max_outflow / total_outflow)
-                if flowx[x, y] < 0:
-                    flowx[x, y] *= scale
-                if flowx[x + 1, y] > 0:
-                    flowx[x + 1, y] *= scale
-                if flowy[x, y] < 0:
-                    flowy[x, y] *= scale
-                if flowy[x, y + 1] > 0:
-                    flowy[x, y + 1] *= scale
-
-    for y in range(N):
-        for x in range(N):
-            water[x, y] += (flowx[x, y] + flowy[x, y] - flowx[x+1, y] - flowy[x, y+1]) * dt/dx/dy
-
-    if step % 500 == 0:
-        ax.imshow(water, cmap='hot', interpolation='nearest')
-        ax.set_title(str(step))
-        #plt.show()
-        plt.pause(0.01)
+    step += 100
+    trigger = False
+    for _ in range(100):
+        simulation.step()
---
+++ water.py
@@ -0,0 +1,54 @@
+import numpy as np
+from vxl_importer import import_vxl
+
+frictionFactor = 0.5
+g = 10
+dx = dy = 10
+dt = 1
+
+class Simulator:
+    def __init__(self, vxl:str):
+        self.terrain, self.water = import_vxl(r'data\lake_fill.txt')
+        self.N = self.terrain.shape[0]
+        self.flowx = np.zeros([self.N + 1, self.N])
+        self.flowy = np.zeros([self.N, self.N + 1])
+
+    def step(self):
+        self.flowx[0, :] = 0
+        self.flowx[self.N, :] = 0
+        self.flowy[:, 0] = 0
+        self.flowy[:, self.N] = 0
+
+        for y in range(self.N):
+            for x in range(1, self.N):
+                self.flowx[x, y] = self.flowx[x, y] * frictionFactor + \
+                              ((self.water[x - 1, y] + self.terrain[x - 1, y]) - (self.water[x, y] + self.terrain[x, y])) * g * dt / dx
+
+        for y in range(1, self.N):
+            for x in range(self.N):
+                self.flowy[x, y] = self.flowy[x, y] * frictionFactor + \
+                              ((self.water[x, y - 1] + self.terrain[x, y - 1]) - (self.water[x, y] + self.terrain[x, y])) * g * dt / dx
+
+        for y in range(self.N):
+            for x in range(self.N):
+                total_outflow = 0
+                total_outflow += max(0, -self.flowx[x, y])
+                total_outflow += max(0, -self.flowy[x, y])
+                total_outflow += max(0, self.flowx[x + 1, y])
+                total_outflow += max(0, self.flowy[x, y + 1])
+
+                if total_outflow > 0:
+                    max_outflow = self.water[x, y] * dx * dy / dt
+                    scale = min(1, max_outflow / total_outflow)
+                    if self.flowx[x, y] < 0:
+                        self.flowx[x, y] *= scale
+                    if self.flowx[x + 1, y] > 0:
+                        self.flowx[x + 1, y] *= scale
+                    if self.flowy[x, y] < 0:
+                        self.flowy[x, y] *= scale
+                    if self.flowy[x, y + 1] > 0:
+                        self.flowy[x, y + 1] *= scale
+
+        for y in range(self.N):
+            for x in range(self.N):
+                self.water[x, y] += (self.flowx[x, y] + self.flowy[x, y] - self.flowx[x + 1, y] - self.flowy[x, y + 1]) * dt / dx / dy
