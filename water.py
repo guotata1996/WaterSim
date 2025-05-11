1ee552fdbe7714c5295c78ea55961eb579edfc98
jguo <jg4006@columbia.edu>
2025-05-10 20:50:08 -0400

    get closer to invariant result under varying time/length resolution

--- water.py
+++ water.py
@@ -2,11 +2,11 @@ import numpy as np
 from itertools import product
 from vxl_importer import import_vxl
 
-inertFactor = 0.8
-g = 10
-dx = 10
-dt = 1
-sourceScale = 0.01
+frictionFactor = 0.2
+g = 0.1
+dx = 1
+dt = 0.1
+sourceRate = 0.1 # m/s
 
 class Simulator:
     def __init__(self, vxl:str):
@@ -22,41 +22,42 @@ class Simulator:
         self.terrain[:,-1] = self.terrain[:,-2]
         self.terrain[0,0] = self.terrain[0,-1] = self.terrain[-1,0] = self.terrain[-1,-1] = -1
 
-        self.water = np.zeros((self.M,self.N))
+        self.water = np.zeros((self.M,self.N))  # m
         self.water[1:-1,1:-1] = water
 
         self.source = {}
         for (x, y), intensity in source.items():
             self.source[(x+1,y+1)] = intensity
 
-        self.flowx = np.zeros([self.M + 1, self.N])
+        self.flowx = np.zeros([self.M + 1, self.N]) #m^3/s
         self.flowy = np.zeros([self.M, self.N + 1])
 
     def step(self):
         for (x, y), intensity in self.source.items():
-            self.water[x,y] += intensity * dt * sourceScale
+            self.water[x,y] += intensity * dt * sourceRate
 
-        boost_x1 = np.maximum(0, -np.sign(self.flowx[1:-1,:])) * (-self.flowx[2:,:]) / np.maximum(self.water[1:,:], 0.001)
+        # boost causes varying result when scaling in length
+        boost_x1 = np.maximum(0, -np.sign(self.flowx[1:-1,:])) * (-self.flowx[2:,:]) / np.maximum(self.water[1:,:], 0.001) # m^2/s
         boost_x2 = np.maximum(0, np.sign(self.flowx[1:-1,:])) * self.flowx[:-2,:] / np.maximum(self.water[:-1,:], 0.001)
-        boost_x = np.maximum(boost_x1, boost_x2)
+        boost_x = np.maximum(boost_x1, boost_x2) / dx
         boost_x = np.minimum(1, boost_x)
         boost_x = np.exp(boost_x)
-        self.flowx[1:-1] = self.flowx[1:-1] * inertFactor + \
-                           boost_x * ((self.water[:-1,:] + self.terrain[:-1:]) - (self.water[1:,:] + self.terrain[1:,:])) * g * dt / dx
+        self.flowx[1:-1] = self.flowx[1:-1] * (1 - frictionFactor * dt) + \
+                           boost_x * ((self.water[:-1,:] + self.terrain[:-1:]) - (self.water[1:,:] + self.terrain[1:,:])) * g * dt * dx
 
         boost_y1 = np.maximum(0, -np.sign(self.flowy[:,1:-1])) * (-self.flowy[:,2:]) / np.maximum(self.water[:,1:], 0.001)
         boost_y2 = np.maximum(0, np.sign(self.flowy[:,1:-1])) * self.flowy[:,:-2] / np.maximum(self.water[:,:-1], 0.001)
-        boost_y = np.maximum(boost_y1, boost_y2)
+        boost_y = np.maximum(boost_y1, boost_y2) / dx
         boost_y = np.minimum(1, boost_y)
         boost_y = np.exp(boost_y)
-        self.flowy[:,1:-1] = self.flowy[:,1:-1] * inertFactor + \
-                             boost_y * ((self.water[:,:-1] + self.terrain[:,:-1]) - (self.water[:,1:] + self.terrain[:,1:])) * g * dt / dx
+        self.flowy[:,1:-1] = self.flowy[:,1:-1] * (1 - frictionFactor * dt) + \
+                             boost_y * ((self.water[:,:-1] + self.terrain[:,:-1]) - (self.water[:,1:] + self.terrain[:,1:])) * g * dt * dx
 
         # Non-parallel part
         scale_order = list(product(range(self.M), range(self.N)))
         np.random.shuffle(scale_order)
         for x, y in scale_order:
-            total_outflow = 0
+            total_outflow = 0  # m^3/s
             total_outflow += max(0, -self.flowx[x, y])
             total_outflow += max(0, -self.flowy[x, y])
             total_outflow += max(0, self.flowx[x + 1, y])
