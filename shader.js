89ea65795355fcce1ed1cb268ad1bf34c9a0ff4b
jguo <jg4006@columbia.edu>
2025-07-28 22:13:45 -0400

    smooth out velocity sample by bi-interpolation

--- shader.js
+++ shader.js
@@ -65,27 +65,35 @@ export async function LoadMaterial()
             uniform int n;
             uniform float uTime;
 
+            float samplevxMap(float localX, float localZ)
+            {
+                float fm = float(m);
+                float fn = float(n);
+                float u = localX / fn;
+                float v = localZ / (fm + 1.0) + 0.5 / (fm + 1.0);
+                return texture2D(vxMap, vec2(u, v)).r;
+            }
+
+            float samplevyMap(float localX, float localZ)
+            {
+                float fm = float(m);
+                float fn = float(n);
+                float u = localX / (fn + 1.0) + 0.5 / (fn + 1.0);
+                float v = localZ / fm;
+                return texture2D(vyMap, vec2(u, v)).r;
+            }
+
             void main() {
                 bool facingX = abs(round(xGrid) - xGrid) != 0.0;
                 bool facingY = abs(round(yGrid) - yGrid) != 0.0;
                 bool isVertical = facingX || facingY;
-
-                float fm = float(m);
-                float fn = float(n);
-                float vxSampleU = vertex_local.x / fn;
-                float vxSampleV = vertex_local.z / (fm + 1.0) + 0.5 / (fm + 1.0);
-
-                float vySampleU = vertex_local.x / (fn + 1.0) + 0.5 / (fn + 1.0);
-                float vySampleV = vertex_local.z / fm;
-
-                float vx = texture2D(vxMap, vec2(vxSampleU, vxSampleV)).r;
-                float vy = texture2D(vyMap, vec2(vySampleU, vySampleV)).r;
 
                 if (isVertical)
                 {
                     if (facingX)
                     {
                         vec2 uv = vertex_local.zy;
+                        float vy = samplevyMap(vertex_local.x, vertex_local.z);
                         if (abs(vy) > 0.0)
                         {
                             vec2 displacement = (uTime - floor(uTime)) * vec2(0.0, max(abs(vy), 1.0));
@@ -99,6 +107,7 @@ export async function LoadMaterial()
                     else
                     {
                         vec2 uv = vertex_local.xy;
+                        float vx = samplevxMap(vertex_local.x, vertex_local.z);
                         if (abs(vx) > 0.0)
                         {
                             vec2 displacement = (uTime - floor(uTime)) * vec2(0.0, max(abs(vx), 1.0));
@@ -106,19 +115,51 @@ export async function LoadMaterial()
                         }
                         else
                         {
-                            
                             gl_FragColor = texture2D(terrainDiffuseMap, uv);
                         }
                     }
                 }
-                else 
-                if (waterDepth < 0.001)
+                else if (waterDepth < 0.001)
                 {
                     gl_FragColor = texture2D(terrainDiffuseMap, vertex_local.xz);
                 }
                 else
                 {
-                    vec2 displacement = (uTime - floor(uTime)) * vec2(-vy, -vx);
+                    float fm = float(m);
+                    float fn = float(n);
+                    float vxSampleUMinus = max(floor(vertex_local.x - 0.5) + 0.5, 0.0);
+                    float vxSampleUPlus = min(vxSampleUMinus + 1.0, fn);
+                    float vxSampleVMinus = max(floor(vertex_local.z), 0.0);
+                    float vxSampleVPlus = min(vxSampleVMinus + 1.0, fm);
+                    float vx00 = samplevxMap(vxSampleUMinus, vxSampleVMinus);
+                    float vx01 = samplevxMap(vxSampleUMinus, vxSampleVPlus);
+                    float vx10 = samplevxMap(vxSampleUPlus, vxSampleVMinus);
+                    float vx11 = samplevxMap(vxSampleUPlus, vxSampleVPlus);
+                    float vxSmooth = mix(
+                        mix(vx00, vx01, vertex_local.z - vxSampleVMinus),
+                        mix(vx10, vx11, vertex_local.z - vxSampleVMinus),
+                        vertex_local.x - vxSampleUMinus);
+
+                    float vySampleUMinus = max(floor(vertex_local.x), 0.0);
+                    float vySampleUPlus = min(vySampleUMinus + 1.0, fn);
+                    float vySampleVMinus = max(floor(vertex_local.z - 0.5) + 0.5, 0.0);
+                    float vySampleVPlus = min(vySampleVMinus + 1.0, fm);
+                    float vy00 = samplevyMap(vySampleUMinus, vySampleVMinus);
+                    float vy01 = samplevyMap(vySampleUMinus, vySampleVPlus);
+                    float vy10 = samplevyMap(vySampleUPlus, vySampleVMinus);
+                    float vy11 = samplevyMap(vySampleUPlus, vySampleVPlus);
+                    float vySmooth = mix(
+                        mix(vy00, vy01, vertex_local.z - vySampleVMinus),
+                        mix(vy10, vy11, vertex_local.z - vySampleVMinus),
+                        vertex_local.x - vySampleUMinus);
+                        
+                    vec2 v = vec2(-vySmooth, -vxSmooth);
+                    const float ClampV = 0.6;
+                    if (length(v) > ClampV)
+                    {
+                        v = v / length(v) * ClampV;
+                    }
+                    vec2 displacement = (uTime - floor(uTime)) * v;
 
                     vec4 color = texture2D(waterDiffuseMap, vertex_local.xz + displacement);
                     color.xy = color.xy * mix(1.0, 0.7, waterDepth);
